d8e684666c7c3b819bd17116760c951a78109f90
kwg <1020296242@qq.com>
2019-03-18 11:50:54 +0800

    新增一些自定义的viewpage

--- app/src/main/java/com/example/myapplication/day2/Main4Activity.java
+++ app/src/main/java/com/example/myapplication/day2/Main4Activity.java
@@ -1,13 +1,8 @@
 package com.example.myapplication.day2;
 
-import android.support.annotation.NonNull;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.myapplication.R;
@@ -22,6 +17,7 @@ public class Main4Activity extends AppCompatActivity {
 
      private int [] ids = new int[]{R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.a,R.drawable.d};
      private List<ImageView> imageViews ;
+    MyPgAdapter adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,39 +30,39 @@ public class Main4Activity extends AppCompatActivity {
             imageView.setBackgroundResource(ids[i]);
             imageViews.add(imageView);
         }
-
-        vp.setAdapter(new myPageAdapter());
+        adapter = new MyPgAdapter(this,imageViews);
+        vp.setAdapter(adapter);
 
 
 
     }
 
 
-    private class myPageAdapter extends PagerAdapter {
-        @NonNull
-        @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-
-            ImageView imageView  = imageViews.get(position);
-            container.addView(imageView);
-            Log.e(TAG,"imageView--"+imageView+"position--"+position);
-            return imageView;
-        }
-
-        @Override
-        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            Log.e(TAG,"object--"+object+"position--"+position);
-               container.removeView((View) object);
-        }
-
-        @Override
-        public int getCount() {
-            return imageViews.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-            return view==o;
-        }
-    }
+//    private class myPageAdapter extends PagerAdapter {
+//        @NonNull
+//        @Override
+//        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+//
+//            ImageView imageView  = imageViews.get(position);
+//            container.addView(imageView);
+//            Log.e(TAG,"imageView--"+imageView+"position--"+position);
+//            return imageView;
+//        }
+//
+//        @Override
+//        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+//            Log.e(TAG,"object--"+object+"position--"+position);
+//               container.removeView((View) object);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return imageViews.size();
+//        }
+//
+//        @Override
+//        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+//            return view==o;
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/myapplication/day2/MyPgAdapter.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.day2;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class MyPgAdapter extends PagerAdapter {
+
+    private Context mcontext;
+    private List<ImageView> imageViews ;
+
+    public MyPgAdapter(Context mcontext, List<ImageView> imageViews) {
+        this.mcontext = mcontext;
+        this.imageViews = imageViews;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+       ImageView imageView = imageViews.get(position);
+       container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return imageViews.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view==o;
+    }
+}
